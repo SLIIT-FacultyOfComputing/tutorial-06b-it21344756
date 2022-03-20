d4232d1e89414a4fbee3a6b8c918515dca7fee62
it21344756 <99312095+it21344756@users.noreply.github.com>
2022-03-20 11:59:51 +0000

    did it

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
   
 // == DO NOT CHANGE THE MAIN PROGRAM =================
-   Student mystd;
-   mystd.assignDetails(1212, "Amalinga");
-   mystd.display();
+   Student std1;
+   std1.assignDetails(1212, "Amalinga");
+   std1.display();
 //==========================
    return 0;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,17 @@
 class Student {
-  // private section
+ // private section
+  private:
+   int studentID ;
+   char studentName[20] ;
   //    int studentId
+ 
   //    name <- 20 charcters
+
   
   // public section
+  public:
   //    assignDetails() method declaration
+void assignDetails(int no,char name[]);
   //    display() method declaration
+void display();
 };
